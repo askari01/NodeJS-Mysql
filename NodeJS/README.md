875110cac44fe09d2e2b36e5857b591289e118d5
Farrukh Askari <farrukh.askari01@gmail.com>
2015-09-12 18:12:16 +0500

    Initial commit; contains SQL file; CURD functions using GET, POST, PUT & DELETE

---
+++ NodeJS/README.md
@@ -0,0 +1,12 @@
+﻿# NodeJS
+
++prerequisite:
+-nodejs
+
++Download/clone the code then:
+
+-at terminal/cmd type: "node Server.js"
+
+-Visit "localhost:3000" to view the app.
+
+-use POSTMAN (chrome extension for header/optional)
---
+++ NodeJS/REST.js
@@ -0,0 +1,85 @@
+﻿//aurthor : farrukh askari
+//help was taaken from blog at codeforgeek.com
+//code taken and modified from github account : codeforgeek
+//twitter : @codeforgeek
+
+var mysql = require("mysql");
+
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes = function(router,connection,md5) {
+    var self = this;
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+    });
+
+    router.get("/users",function(req,res){
+        var query = "SELECT * FROM ??";
+        var table = ["table1"];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+
+    router.get("/users/:user_id",function(req,res){
+        var query = "SELECT * FROM ?? WHERE ??=?";
+        var table = ["user_login","user_id",req.params.user_id];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+            }
+        });
+    });
+
+    router.post("/users",function(req,res){
+        var query = "INSERT INTO ??(??,??,??,??) VALUES (?,?,?,?)";
+        var table = ["table1","name","fname","user", "password", req.body.name, , req.body.fname, req.body.user, md5(req.body.password)];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+
+    router.put("/users",function(req,res){
+        var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        var table = ["table1","password",md5(req.body.password),"user",req.body.user];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Updated the password for email "+req.body.email});
+            }
+        });
+    });
+
+    router.delete("/users/:email",function(req,res){
+        var query = "DELETE from ?? WHERE ??=?";
+        var table = ["user_login","user_email",req.params.email];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "Deleted the user with email "+req.params.email});
+            }
+        });
+    });
+}
+
+module.exports = REST_ROUTER;
